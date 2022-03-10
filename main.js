2bcf15b87b1073f748808cfdc10b9f6bed49c71a
Daniel Castro <danielcastrofafe@gmail.com>
2022-03-10 21:12:58 +0000

    Updated sw

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/ex1/sw.js', { scope: '/ex1/.' })
+    .register('./sw.js', { scope: './' })
     .then((reg) => {
       console.log('Registration suceeded. Scope is' + reg.scope);
     })
